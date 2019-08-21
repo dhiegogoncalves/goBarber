dad75b9a601ef6467868aa744c5e5c0db4b2eeec
Dhiego Gonçalves da Silva <dhhiego@gmail.com>
2019-08-21 16:53:58 -0400

    Estilização da autenticação

--- front-end/src/pages/SignUp/index.js
+++ front-end/src/pages/SignUp/index.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '../../assets/logo.svg';
 
 export default function SignUp() {
-  return <h1>SignUp</h1>;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+      <form>
+        <input type="text" placeholder="Nome completo" />
+        <input type="text" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  );
 }
