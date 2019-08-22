2604d8c4e5cc73d031afd77f326ffa1fa6f330a6
Dhiego Gonçalves da Silva <dhhiego@gmail.com>
2019-08-22 16:58:06 -0400

    Estilizando notificações

--- front-end/src/components/Notifications/index.js
+++ front-end/src/components/Notifications/index.js
@@ -1,9 +1,70 @@
 import React from 'react';
 
-import {} from 'react-icons';
+import { MdNotifications } from 'react-icons/md';
 
-import { Container } from './styles';
+import {
+  Container,
+  Badge,
+  NotificationsList,
+  Scroll,
+  Notification,
+} from './styles';
 
 export default function Notifications() {
-  return <div></div>;
+  return (
+    <Container>
+      <Badge hasUnread>
+        <MdNotifications color="#7159c1" size={20} />
+      </Badge>
+      <NotificationsList>
+        <Scroll>
+          <Notification unread>
+            <p>Você possui um novo agendamento para amanhã</p>
+            <time>há 2 dias</time>
+            <button type="button">Marca como lida</button>
+          </Notification>
+          <Notification>
+            <p>Você possui um novo agendamento para amanhã</p>
+            <time>há 2 dias</time>
+            <button type="button">Marca como lida</button>
+          </Notification>
+          <Notification>
+            <p>Você possui um novo agendamento para amanhã</p>
+            <time>há 2 dias</time>
+            <button type="button">Marca como lida</button>
+          </Notification>
+          <Notification>
+            <p>Você possui um novo agendamento para amanhã</p>
+            <time>há 2 dias</time>
+            <button type="button">Marca como lida</button>
+          </Notification>
+          <Notification>
+            <p>Você possui um novo agendamento para amanhã</p>
+            <time>há 2 dias</time>
+            <button type="button">Marca como lida</button>
+          </Notification>
+          <Notification>
+            <p>Você possui um novo agendamento para amanhã</p>
+            <time>há 2 dias</time>
+            <button type="button">Marca como lida</button>
+          </Notification>
+          <Notification>
+            <p>Você possui um novo agendamento para amanhã</p>
+            <time>há 2 dias</time>
+            <button type="button">Marca como lida</button>
+          </Notification>
+          <Notification>
+            <p>Você possui um novo agendamento para amanhã</p>
+            <time>há 2 dias</time>
+            <button type="button">Marca como lida</button>
+          </Notification>
+          <Notification>
+            <p>Você possui um novo agendamento para amanhã</p>
+            <time>há 2 dias</time>
+            <button type="button">Marca como lida</button>
+          </Notification>
+        </Scroll>
+      </NotificationsList>
+    </Container>
+  );
 }
--- front-end/src/components/Notifications/styles.js
+++ front-end/src/components/Notifications/styles.js
@@ -1,65 +1,98 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import PerfectScrollBar from 'react-perfect-scrollbar';
+import { lighten } from 'polished';
 
 export const Container = styled.div`
-  background: #fff;
-  padding: 0 30px;
+  position: relative;
 `;
 
-export const Content = styled.div`
-  height: 64px;
-  max-width: 900px;
-  margin: 0 auto;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+export const Badge = styled.button`
+  background: none;
+  border: 0;
+  position: relative;
 
-  nav {
-    display: flex;
-    align-items: center;
+  ${props =>
+    props.hasUnread &&
+    css`
+      &::after {
+        position: absolute;
+        right: 0;
+        top: 0;
+        width: 8px;
+        height: 8px;
+        background: #ff892e;
+        content: '';
+        border-radius: 50%;
+      }
+    `}
+`;
 
-    img {
-      margin-right: 20px;
-      padding-right: 20px;
-      border-right: 1px solid #eee;
-    }
+export const NotificationsList = styled.div`
+  position: absolute;
+  width: 260px;
+  left: calc(50% - 130px);
+  top: calc(100% + 30px);
+  background: rgba(0, 0, 0, 0.6);
+  border-radius: 4px;
+  padding: 15px 5px;
 
-    a {
-      font-weight: bold;
-      color: #7159c1;
-    }
-  }
-
-  aside {
-    display: flex;
-    align-items: center;
+  &::before {
+    content: '';
+    position: absolute;
+    left: calc(50% - 20px);
+    top: -20px;
+    width: 0;
+    height: 0;
+    border-left: 20px solid transparent;
+    border-right: 20px solid transparent;
+    border-bottom: 20px solid rgba(0, 0, 0, 0.6);
   }
 `;
 
-export const Profile = styled.div`
-  display: flex;
-  margin-left: 20px;
-  padding-left: 20px;
-  border-left: 1px solid #eee;
+export const Scroll = styled(PerfectScrollBar)`
+  max-height: 260px;
+  padding: 5px 15px;
+`;
 
-  div {
-    text-align: right;
-    margin-right: 10px;
+export const Notification = styled.div`
+  color: #fff;
 
-    strong {
-      display: block;
-      color: #333;
-    }
-
-    a {
-      display: block;
-      margin-top: 2px;
-      font-size: 12px;
-      color: #999;
-    }
+  & + div {
+    margin-top: 15px;
+    padding-top: 15px;
+    border-top: 1px solid rgba(255, 255, 255, 0.1);
   }
 
-  img {
-    height: 32px;
-    border-radius: 50%;
+  p {
+    font-size: 13px;
+    line-height: 18px;
   }
+
+  time {
+    font-size: 12px;
+    opacity: 0.6;
+  }
+
+  button {
+    font-size: 12px;
+    border: 0;
+    background: none;
+    color: ${lighten(0.2, '#7159c1')};
+    padding: 0 5px;
+    margin: 0 5px;
+    border-left: 1px solid rgba(255, 255, 255, 0.1);
+  }
+
+  ${props =>
+    props.unread &&
+    css`
+      &::after {
+        content: '';
+        display: inline-block;
+        width: 8px;
+        height: 8px;
+        background: #ff892e;
+        border-radius: 50%;
+      }
+    `}
 `;
