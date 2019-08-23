a206c94146b9683033705781a7fe0764330ef0c0
Dhiego Gonçalves da Silva <dhhiego@gmail.com>
2019-08-23 12:17:05 -0400

    Foto de perfil

--- front-end/src/pages/Profile/index.js
+++ front-end/src/pages/Profile/index.js
@@ -4,18 +4,21 @@ import { Form, Input } from '@rocketseat/unform';
 
 import { Container } from './styles';
 import { updateProfileRequest } from '../../store/modules/user/actions';
+import AvatarInput from './AvatarInput';
 
 export default function Profile() {
   const dispatch = useDispatch();
   const profile = useSelector(state => state.user.profile);
 
   function handleSubmit(data) {
+    console.log(data);
     dispatch(updateProfileRequest(data));
   }
 
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
         <hr />
