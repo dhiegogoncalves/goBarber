7e4086c71f467664e1b6abaa6e7b7da683c76f68
Dhiego Gonçalves da Silva <dhhiego@gmail.com>
2019-08-25 16:39:19 -0400

    Conectando Redux

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,8 +1,10 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Image } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 
 import logo from '../../assets/logo.png';
 import Background from '../../components/Background';
+import { signInRequest } from '../../store/modules/auth/actions';
 
 import {
   Container,
@@ -14,9 +16,17 @@ import {
 } from './styles';
 
 export default function SignIn({ navigation }) {
+  const dispatch = useDispatch();
   const passwordRef = useRef();
 
-  async function handleSubmit() {}
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const loading = useSelector(state => state.auth.loading);
+
+  async function handleSubmit() {
+    dispatch(signInRequest(email, password));
+  }
 
   return (
     <Background>
@@ -32,6 +42,8 @@ export default function SignIn({ navigation }) {
             placeholder="Digite seu e-mail"
             returnKeyType="next"
             onSubmitEditing={() => passwordRef.current.focus()}
+            value={email}
+            onChangeText={setEmail}
           />
 
           <FormInput
@@ -40,10 +52,14 @@ export default function SignIn({ navigation }) {
             placeholder="Sua senha secreta"
             ref={passwordRef}
             returnKeyType="send"
+            value={password}
+            onChangeText={setPassword}
             onSubmitEditing={handleSubmit}
           />
 
-          <SubmitButton onPress={handleSubmit}>Acessar</SubmitButton>
+          <SubmitButton loading={loading} onPress={handleSubmit}>
+            Acessar
+          </SubmitButton>
         </Form>
 
         <SignLink onPress={() => navigation.navigate('SignUp')}>
--- mobile/src/store/modules/auth/sagas.js
+++ mobile/src/store/modules/auth/sagas.js
@@ -1,4 +1,4 @@
-import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { takeLatest, call, put, all, delay } from 'redux-saga/effects';
 import { Alert } from 'react-native';
 
 import api from '../../../services/api';
@@ -25,6 +25,8 @@ export function* signIn({ payload }) {
 
     api.defaults.headers.Authorization = `Bearer ${token}`;
 
+    // yield delay(3000);
+
     yield put(signInSuccess(token, user));
     //history.push('/dashboard');
   } catch (error) {
@@ -44,7 +46,6 @@ export function* signUp({ payload }) {
       name,
       email,
       password,
-      provider: true,
     });
     //history.push('/');
     Alert.alert('Sucesso', 'Conta criada com sucesso');
