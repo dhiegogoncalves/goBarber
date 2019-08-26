4f9b968e65cb00d91b825b1ebbd35d54a7f24b73
Dhiego Gonçalves da Silva <dhhiego@gmail.com>
2019-08-25 20:01:52 -0400

    Listagem de prestadores

--- mobile/src/pages/New/SelectDateTime/index.js
+++ mobile/src/pages/New/SelectDateTime/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-const index = () => {
-  return (
-    <View>
-      <Text></Text>
-    </View>
-  );
-};
+import api from '../../../services/api';
+import Background from '../../../components/Background';
+import { Container, Providerlist, Provider, Avatar, Name } from './styles';
 
-export default index;
+export default function SelectDateTime({ navigation }) {
+  const [providers, setProviders] = useState([]);
+
+  useEffect(() => {}, []);
+
+  return <Background></Background>;
+}
+
+SelectDateTime.navigationOptions = ({ navigation }) => ({
+  title: 'Selecione o horário',
+  headerLeft: () => (
+    <TouchableOpacity onPress={() => navigation.navigate('SelectProvider')}>
+      <Icon name="chevron-left" size={20} color="#fff" />
+    </TouchableOpacity>
+  ),
+});
